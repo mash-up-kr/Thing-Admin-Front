67aa5231503b3915a7379e1db7cc0f1c17524b1f
korea8378 <korea8378@naver.com>
2019-08-14 21:18:45 +0900

    [#2][ADD] Select(main) 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export { default as Home } from './Home';
 export { default as Select } from './Select';
+export { default as Category } from './Category';
+export { default as Youtuber } from './Youtuber';
+export { default as Tag } from './Tag';
 export { default as PrivateRoute } from './PrivateRoute';
